d2a7b56d3b2df73a12ea111768b8d04cd3c12315
Steve Frampton <40193522+FarmingtonS9@users.noreply.github.com>
2026-03-02 15:28:19 +1030

    update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
+// const PROTON: Particle = Particle {
+//     p_type: ParticleType::Proton,
+//     charge: ChargeType::Positive,
+// };
+// const NEUTRON: Particle = Particle {
+//     p_type: ParticleType::Neutron,
+//     charge: ChargeType::Neutral,
+// };
+// const ELECTRON: Particle = Particle {
+//     p_type: ParticleType::Electron,
+//     charge: ChargeType::Negative,
+// };
+
 fn main() {
     println!("Hello, world!");
 
     // U235
     let u_235 = Atom {
-        proton: Proton { number: 92 },
-        neutron: Neutron { number: 143 },
-        electron: Electron { number: 92 },
+        proton: Particle::proton(92),
+        neutron: Particle::neutron(143),
+        electron: Particle::electron(92),
+        position: Position::origin(),
     };
 
     println!("Nucleon: {}", u_235.nucleon());
@@ -20,38 +34,89 @@ fn main() {
     }
 }
 
-struct Neutron {
+struct Particle {
+    p_type: ParticleType,
     number: u8,
 }
+impl Particle {
+    fn proton(number: u8) -> Self {
+        Self {
+            p_type: ParticleType::Proton,
+            number,
+        }
+    }
+    fn neutron(number: u8) -> Self {
+        Self {
+            p_type: ParticleType::Neutron,
+            number,
+        }
+    }
+    fn electron(number: u8) -> Self {
+        Self {
+            p_type: ParticleType::Electron,
+            number,
+        }
+    }
+    fn positron(number: u8) -> Self {
+        Self {
+            p_type: ParticleType::Electron,
+            number,
+        }
+    }
+}
 
+// May change to hadrons and baryons.
+enum ParticleType {
+    Proton,
+    Neutron,
+    Electron,
+}
+
+enum ChargeType {
+    Positive,
+    Negative,
+    Neutral,
+}
+
+struct Neutron {
+    properties: NeutronProperties,
+}
 struct NeutronProperties {
     energy: Energy, //Measured in eV
+    mass: Mass,
+    charge: ChargeType,
 }
 impl Default for NeutronProperties {
     fn default() -> Self {
         Self {
             energy: Energy(0_f64),
+            mass: Mass(1_f64),
+            charge: ChargeType::Neutral,
         }
     }
 }
-
-struct Proton {
-    number: u8,
-}
-
-struct Electron {
-    number: u8,
-}
-
 struct Atom {
-    proton: Proton,
-    neutron: Neutron,
-    electron: Electron,
+    proton: Particle,
+    neutron: Particle,
+    electron: Particle,
+    position: Position,
 }
 impl Atom {
-    fn nucleon(&self) -> u8 {
-        self.proton.number + self.neutron.number
+    fn nucleon(&self) -> u16 {
+        (self.proton.number + self.neutron.number).into()
     }
 }
 
 struct Energy(f64);
+
+struct Mass(f64);
+
+struct Position {
+    x: i32,
+    y: i32,
+}
+impl Position {
+    fn origin() -> Self {
+        Self { x: 0, y: 0 }
+    }
+}
